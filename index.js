cd347ae17c035b47e2ea954b1cdc6d99267e13ad
Steven Lu <tacticalazn@gmail.com>
2016-05-24 12:28:58 -0400

    updating options

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var PluginError = gutil.PluginError
 var FastlyPurge = require('fastly-purge')
 
 module.exports = function (options) {
-  var fastlyPurge = new FastlyPurge(options.FASTLY_API_KEY)
+  var fastlyPurge = new FastlyPurge(options.apiKey)
 
   var invalidate = function (cb) {
-    fastlyPurge.service(options.FASTLY_SERVICE_ID, cb)
+    fastlyPurge.service(options.serviceId, cb)
   }
 
   return through.obj(function (file, enc, cb) {
